ce2870d38dafe31e97deca646e80531c53946415
Amitavpanda <pandaamitav01@gmail.com>
2021-11-14 11:48:48 +0530

    Solution of Permutations.py

---
+++ Permutations.py
@@ -0,0 +1,14 @@
+n = int(input())
+if n == 1:
+    print(1)
+elif n >= 4 :
+    l = []
+    for i in range(1,n+1):
+        if i % 2 == 0:
+            print(i ,end=" ")
+    for i in range(1,n+1):
+        if i % 2 != 0:
+            print(i ,end=" ")
+else:
+    print("NO SOLUTION")
+            
